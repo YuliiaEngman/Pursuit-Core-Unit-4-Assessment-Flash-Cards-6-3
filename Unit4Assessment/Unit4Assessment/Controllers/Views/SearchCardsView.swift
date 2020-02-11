65b67c0e92a3d25752ee7b29f2e6edf83578bfc7
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-11 12:45:21 -0500

    setup search view layout

--- Unit4Assessment/Unit4Assessment/Controllers/Views/SearchCardsView.swift
+++ Unit4Assessment/Unit4Assessment/Controllers/Views/SearchCardsView.swift
@@ -10,12 +10,55 @@ import UIKit
 
 class SearchCardsView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    public lazy var searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.autocapitalizationType = .none
+        sb.placeholder = "search for cards"
+        return sb
+    }()
+    
+    public lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundColor = .systemGreen
+        return cv
+    }()
+
+        override init(frame: CGRect) {
+            super.init(frame: UIScreen.main.bounds)
+            commonInit()
+        }
+        
+        required init?(coder: NSCoder) {
+            super .init(coder: coder)
+            commonInit()
+        }
+        
+        private func commonInit(){
+            setupSearchBarConstraints()
+            setupLayoutConstraints()
+        }
+        
+    private func setupSearchBarConstraints() {
+         addSubview(searchBar)
+         searchBar.translatesAutoresizingMaskIntoConstraints = false
+         NSLayoutConstraint.activate([
+             searchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+             searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+             searchBar.trailingAnchor.constraint(equalTo: trailingAnchor)
+         ])
+     }
+    
+    private func setupLayoutConstraints() {
+         addSubview(collectionView)
+         collectionView.translatesAutoresizingMaskIntoConstraints = false
+         NSLayoutConstraint.activate([
+             collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+             collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+             collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+             collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+         ])
+     }
 
 }
